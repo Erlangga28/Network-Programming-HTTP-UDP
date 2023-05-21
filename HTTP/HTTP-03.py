b5dd716ca175188160affd75654a73ddd9aac725
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2023-05-21 21:19:37 +0700

    Edit

--- HTTP/HTTP-03.py
+++ HTTP/HTTP-03.py
@@ -8,24 +8,19 @@ with socket.create_connection((hostname, 443)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         print(ssock.version())
 
-        # prepare request header
         request_header = 'GET / HTTP/1.0\r\nHost: ' + hostname + '\r\n' +  '\r\n\r\n'
         request_header = request_header.encode()
 
-        # make a request
         ssock.send(request_header)
 
-        # receive response from server
         response = b'' 
         while True:
             received = ssock.recv(1024)
             if not received:
                 break
             response += received
-        
-        # parse header and content    
+           
         header, content = response.split(bytes('\r\n\r\n', 'utf-8'), 1)
         header = header.decode()
 
-        # print header and content text
         print(header)
