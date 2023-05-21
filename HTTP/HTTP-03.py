15fbc29e2f9cacff34fcde480d99564ea236fc24
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2023-05-21 21:17:37 +0700

    HTTP No.3

---
+++ HTTP/HTTP-03.py
@@ -0,0 +1,31 @@
+import socket
+import ssl
+
+hostname = 'www.its.ac.id'
+context = ssl.create_default_context()
+
+with socket.create_connection((hostname, 443)) as sock:
+    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+        print(ssock.version())
+
+        # prepare request header
+        request_header = 'GET / HTTP/1.0\r\nHost: ' + hostname + '\r\n' +  '\r\n\r\n'
+        request_header = request_header.encode()
+
+        # make a request
+        ssock.send(request_header)
+
+        # receive response from server
+        response = b'' 
+        while True:
+            received = ssock.recv(1024)
+            if not received:
+                break
+            response += received
+        
+        # parse header and content    
+        header, content = response.split(bytes('\r\n\r\n', 'utf-8'), 1)
+        header = header.decode()
+
+        # print header and content text
+        print(header)
