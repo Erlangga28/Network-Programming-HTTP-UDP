9198fb63d9ce55899383383570a4b6ba534ab8c0
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2023-05-21 20:36:32 +0700

    HTTP No.1

---
+++ HTTP/HTTP-01.py
@@ -0,0 +1,13 @@
+import requests
+
+import http.client
+
+connection = http.client.HTTPSConnection("www.its.ac.id")
+connection.request("GET", "/")
+response = connection.getresponse()
+header = response.getheaders()
+
+print("Status:", response.status, response.reason)
+print("Response header:")
+for item in header:
+    print(item)
