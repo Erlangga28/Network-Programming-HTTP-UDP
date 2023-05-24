92e0901c2c39e5326a4b54e45cdda65922a372f4
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2023-05-24 23:47:30 +0700

    Nno 7 & no 8

---
+++ FTP/FTP-07.py
@@ -0,0 +1,24 @@
+from ftplib import FTP
+from ftplib import FTP_TLS
+
+f = FTP()
+f.set_pasv(False)
+ff = FTP_TLS()
+
+f.connect(host='127.0.0.1')
+res = f.getwelcome()
+print(res.split('\n',1)[0])
+
+res = f.sendcmd("User Erlangga")
+print(res)
+
+res = f.sendcmd("PASS 1234")
+print(res)
+
+res = f.sendcmd('RNFR test')
+print(res)
+res =f.sendcmd('RNTO test2')
+print(res)
+
+res = f.sendcmd("QUIT")
+print(res)
